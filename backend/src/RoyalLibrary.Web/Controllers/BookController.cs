49540fc12a4c035afb2c089ff4c5470c9b95480d
fpessoto <felipe.pessoto@hotmail.com>
2023-03-31 06:57:56 -0300

    clean backend code

--- backend/src/RoyalLibrary.Web/Controllers/BookController.cs
+++
@@ -1,42 +0,0 @@
-﻿using RoyalLibrary.SharedKernel.Interfaces;
-using RoyalLibrary.Web.ViewModels;
-using Microsoft.AspNetCore.Mvc;
-using RoyalLibrary.Core.BooksAggregate;
-using RoyalLibrary.Core.BooksAggregate.Specifications;
-using RoyalLibrary.Web.ApiModels;
-
-namespace RoyalLibrary.Web.Controllers;
-
-[Route("[controller]")]
-public class BookController : Controller
-{
-  private readonly IRepository<Book> _repository;
-
-  public BookController(IRepository<Book> repository)
-  {
-    _repository = repository;
-  }
-
-  public async Task<IActionResult> Index()
-  {
-
-    var books = (await _repository.ListAsync());
-
-    var booksDTO = books
-        .Select(book => new BookDTO
-        (
-            id: book.Id,
-            title: book.Title,
-            category: book.Category?.Name ?? "",
-            bookType: book.BookType?.Name ?? "",
-            authorFirstName: book.FirstName,            authorLastName: book.LastName,            isbn: book.Isbn,            totalCopies: book.TotalCopies,            copiesInUse: book.CopiesInUse        ));
-
-
-    var dto = new BookViewModel
-    {
-      Books = booksDTO.ToList()
-    };
-
-    return View(dto);
-  }
-}
